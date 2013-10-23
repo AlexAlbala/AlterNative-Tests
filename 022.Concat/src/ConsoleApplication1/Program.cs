5e83b366edcf6839836c51b958c02e34c48e576d
Gerard <gerard@gsole.cat>
2013-10-23 16:57:31 +0200

    +++ Concat source

---
+++ 022.Concat/src/ConsoleApplication1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String a = "hola";
+            String b = "que";
+            String c = "tal";
+
+            string[] array = new string[2];
+            array[0] = b;
+            array[1] = c;
+            a = String.Concat(array);
+            Console.WriteLine(a);
+        }
+    }
+}
