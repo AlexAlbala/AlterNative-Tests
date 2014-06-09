99d1493e6f7178a19f70a801677a323f3cd393c7
AlexAlbala <alexalbala88@gmail.com>
2014-06-09 16:56:44 +0200

    +++ Added 033.Structs test +++ Updated target files of test

---
+++ 033.Structs/src/Structs/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Structs
+{
+    struct Coordinate
+    {
+        public int x;
+        public int y;
+
+        public Coordinate(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    class Program
+    {
+        public static void f(Coordinate c)
+        {
+            c.x = 3;
+            c.y = 4;
+            Console.WriteLine("c: " + c.x + "  " + c.y);
+        }
+
+        public static void Main(string[] args)
+        {
+
+            Coordinate c = new Coordinate(1,2);
+            //c.x = 1;
+            //c.y = 2;
+            f(c);
+            Console.WriteLine("c: " + c.x + "  " + c.y);
+        }
+    }
+}
